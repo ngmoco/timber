1381186557c32d494d675d9852ac1a461b5ebca9
Dave Grijalva <grijalva@gmail.com>
2013-06-25 11:15:39 -0700

    preprocess filename through a template

--- file_writer.go
+++ file_writer.go
@@ -1,11 +1,14 @@
 package timber
 
 import (
+	"bytes"
 	"compress/gzip"
 	"fmt"
 	"io"
 	"os"
 	"strings"
+	"text/template"
+	"time"
 )
 
 /* unbuffered impl
@@ -31,9 +34,34 @@ func (fw *FileWriter) Close() {
 }
 */
 
+type FilenameFields struct {
+	Hostname string
+	Date     time.Time
+	Pid      int
+}
+
+func GetFilenameFields() *FilenameFields {
+	h, _ := os.Hostname()
+	return &FilenameFields{
+		Hostname: h,
+		Date:     time.Now(),
+		Pid:      os.Getpid(),
+	}
+}
+
+func preprocessFilename(name string) string {
+	t := template.Must(template.New("filename").Parse(name))
+	buf := new(bytes.Buffer)
+	t.Execute(buf, GetFilenameFields())
+	return buf.String()
+}
+
 // This writer has a buffer that I don't ever bother to flush, so it may take a while
 // to see messages.  Filenames ending in .gz will automatically be compressed on write.
+// Filename string is proccessed through the template library using the FilenameFields
+// struct.
 func NewFileWriter(name string) (LogWriter, error) {
+	name = preprocessFilename(name)
 	file, err := os.OpenFile(name, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
 	if err != nil {
 		return nil, fmt.Errorf("TIMBER! Can't open %v: %v", name, err)
